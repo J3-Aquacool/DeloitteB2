d4927bc6b3f43a3b1c4b0e4c3dd5d9d0a4a9b04c
J3-Aquacool <42802228+J3-Aquacool@users.noreply.github.com>
2024-01-30 15:16:53 +0530

    Prepared and Callable Statement

---
+++ ProceduresDemo.java
@@ -0,0 +1,49 @@
+package com.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.sql.CallableStatement;
+
+public class ProceduresDemo {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3305/p9batch","root","admin");
+    	System.out.println("connected");
+    	// caling store procedure
+    	CallableStatement cst=c.prepareCall("{showData(?,?)}");
+    	 
+    	cst.setInt(1, 105);
+    	 
+    	 cst.registerOutParameter(2,Types.VARCHAR);
+     	    	 
+    	   	 
+    	 cst.execute();
+    	 
+    	System.out.println(cst.getString("empname"));
+    	
+    	 
+    	 
+    	 
+    	 
+    	 
+    	 
+    	 
+    	 
+    	 
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+
+
+    	
+	}
+
+}
